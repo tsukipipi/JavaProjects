43a6d8947a3b3af19432fc65b773efee0163901f
tsukipipi <695886283@qq.com>
2018-04-03 23:39:03 +0800

    complete

--- Spring/soundsystem/src/soundsystem6/Test.java
+++ Spring/soundsystem/src/soundsystem6/Test.java
@@ -1,6 +1,7 @@
 package soundsystem6;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
@@ -10,10 +11,12 @@ public class Test {
         ApplicationContext context = new ClassPathXmlApplicationContext("soundsystem6/Config.xml");
         CompactDisc cd1 = (CompactDisc) context.getBean("sgtPeppers");
         CompactDisc cd2 = (CompactDisc) context.getBean("blankDisc");
-        //MediaPlayer player = (MediaPlayer) context.getBean("cdPlayer");
+        //声明Spring上下文，采用java配置类
+//        ApplicationContext context1 = new AnnotationConfigApplicationContext(Config.class);
+//        MediaPlayer player = (MediaPlayer) context1.getBean("cdPlayer");
         cd1.play();
         cd2.play();
-        //player.play();
+//        player.play();
     }
 
 }
