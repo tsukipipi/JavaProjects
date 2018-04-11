221e96b45b1fe8f107fc82930c93c9e991aaba11
tsukipipi <695886283@qq.com>
2018-04-11 14:04:44 +0800

    mvn package

--- Maven/NumberGenerat/src/test/java/com/test/AppTest.java
+++ Maven/NumberGenerat/src/test/java/com/test/AppTest.java
@@ -1,43 +1,14 @@
 package com.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
-    @org.junit.Test
+    @Test
     public void testLengthOfTheUniqueKey() {
 
         App obj = new App();
